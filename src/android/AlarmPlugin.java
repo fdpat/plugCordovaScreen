011956575ad50b7c3959e872bdda18134dd5f73e
fdyan <yan.boisjoly@fdcanada.ca>
2014-12-18 19:04:26 -0500

    Testing multiThread for multiple alarms

--- src/android/AlarmPlugin.java
+++ src/android/AlarmPlugin.java
@@ -90,9 +90,10 @@ public class AlarmPlugin extends CordovaPlugin {
 				PendingIntent alarmIntent;     
 				Intent intent = new Intent(this.cordova.getActivity(), AlarmReceiver.class);
 				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-				alarmIntent = PendingIntent.getBroadcast(this.cordova.getActivity(), id, intent, 0);
+				alarmIntent = PendingIntent.getBroadcast(this.cordova.getActivity(), id, intent, PendingIntent.FLAG_CANCEL_CURRENT);//.cancel();
 				
 				alarmMgr.cancel(alarmIntent);
+				callbackContext.success("Alarm unset, id: " +id);
 				return true;
 			}
 			return false;
