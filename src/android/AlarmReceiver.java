40478a8d9b8567dd9f8b4248815734ee67d90a0b
fdyan <yan.boisjoly@fdcanada.ca>
2014-12-22 12:25:28 -0500

    Modif page redirection

--- src/android/AlarmReceiver.java
+++ src/android/AlarmReceiver.java
@@ -34,7 +34,7 @@ public class AlarmReceiver extends BroadcastReceiver {
         intent.setAction("com.uniclau.alarmplugin.ALARM");
         intent.setPackage(context.getPackageName());
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.putExtra("url", "file:///android_asset/www/index2.html");
+        intent.putExtra("url", "file:///android_asset/www/wakescreen.html");
         context.startActivity(intent);
     }
 }
--- www/alarmplugin.js
+++ www/alarmplugin.js
@@ -20,6 +20,15 @@ var alarm = {
             "unsetAlarm",
             [{"ringId": id}]
         );
+    },
+	unsetAll: function(id, successCallback, errorCallback){
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            "AlarmPlugin",
+            "unsetAlarmAll",
+            [{"arrayId": id}]
+        );
     }
 };
 module.exports = alarm;
